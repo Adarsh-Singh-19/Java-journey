616fcd16751acbc846fb26164a6340132901dc40
Adarsh Singh <your-github-email@example.com>
2026-06-01 10:39:33 +0530

    Day 01 code

---
+++ Day-01/Notes.md
@@ -0,0 +1,7 @@
+## Day 01
+### Topics
+- Java Introduction
+- JDK, JRE, JVM
+- Program Structure
+- Hello World Program
+
---
+++ Day-01/Variable.java
@@ -0,0 +1,17 @@
+public class Variable {
+    
+    public static void main(String[] args) {
+        int a = 10; // integer variable
+        double b = 3.14; // double variable
+        char c = 'A'; // character variable
+        String d = "Hello"; // string variable
+        Boolean e = true; // boolean variable
+
+        System.out.println("Integer variable: " + a);
+        System.out.println("Double variable: " + b);
+        System.out.println("Character variable: " + c);
+        System.out.println("String variable: " + d);
+        System.out.println("Boolean variable: " + e);
+    }
+    
+}
