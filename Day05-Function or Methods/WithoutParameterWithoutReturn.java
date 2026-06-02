06e578f0c1f0c89e92b9af77438294a97108f389
Adarsh Singh <your-github-email@example.com>
2026-06-02 23:38:44 +0530

    methodscode

---
+++ Day05-Function or Methods/WithoutParameterWithoutReturn.java
@@ -0,0 +1,10 @@
+public class WithoutParameterWithoutReturn {
+    public static void main(String[] args) {
+        greet();
+    }
+
+    // Function without parameters and without return type
+    public static void greet() {
+        System.out.println("Hello! Welcome to Java programming.");
+    }
+}
