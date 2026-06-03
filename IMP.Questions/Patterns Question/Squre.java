de2e6e291fc352010d270176a19e03269c421a55
Adarsh Singh <your-github-email@example.com>
2026-06-03 12:05:37 +0530

    method complition1

--- IMP.Questions/Patterns Question/Squre.java
+++ IMP.Questions/Patterns Question/Squre.java
@@ -5,7 +5,8 @@ public static void main(String[]args){
     System.out.print("Enter the number of rows: ");
     int rows = p.nextInt();
     System.out.print("Enter the number of columns: ");
-    int columns = p.nextInt(); 
+    int columns = p.nextInt();
+    p.close(); 
     for(int i=0;i<rows;i++){
         for(int j=0;j<columns;j++){
             System.out.print("* ");
