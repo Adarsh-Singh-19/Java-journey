830ad574539b971ea64cc30eebde3f40a0373fae
Adarsh Singh <your-github-email@example.com>
2026-07-11 23:10:08 +0530

    code

--- Day 09-Sorting/Bubble Sort/Bubble.java
+++ Day 09-Sorting/Bubble Sort/Bubble.java
@@ -1,30 +1,46 @@
 import java.util.*;
-public class Bubble{
 
-    public static void bubbleshort(int []arr){
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+public class Bubble {
+
+    public static void bubbleSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = 0; j < arr.length - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
-
             }
         }
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]);
+
+        System.out.println("Sorted Array:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the size of Array");
-        int n=sc.nextInt();
-        int arr[]=new int[n];
-        System.out.println("Enter the Array's element");
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of Array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter the array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-        bubbleshort(arr);
+
+        bubbleSort(arr);
+
+        sc.close();
     }
 }
